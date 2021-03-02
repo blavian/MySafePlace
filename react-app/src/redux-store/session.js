25f75ef09fe5ae65085d75e1a6f455c78b1a0e7d
Benjamin Lavian <benylavian@gmail.com>
2021-03-02 15:34:14 -0500

    session.js first attempt

--- react-app/src/redux-store/session.js
+++ react-app/src/redux-store/session.js
@@ -1,53 +1,87 @@
-// So we do not make a type we set this to a variable
+// So we do not make a type we set this to a variable 
 const SET_SESSION_USER = 'SET_SESSION_USER'
-const REMOVE_SESSION_USER = 'SET_SESSION_USER'
+const REMOVE_SESSION_USER = 'REMOVE_SESSION_USER'
 
-export const authenticate = async () => {
-  const response = await fetch("/api/auth/", {
+//Action Creators that return objects
+
+const setSessionUser = (user)=>({
+    type:SET_SESSION_USER,
+    user
+})
+
+const removeSessionUser = ()=>({
+    type:REMOVE_SESSION_USER
+})
+
+//Thunks(actions that return functions)
+export const authenticate = () => async (dispatch) => {
+  const res = await fetch("/api/auth/", {
     headers: {
       "Content-Type": "application/json",
     },
   });
-  return await response.json();
+  const data =  await res.json();
+  if (!data.errors) dispatch(setSessionUser(user));
+  return data
 };
 
-export const login = async (email, password) => {
-  const response = await fetch("/api/auth/login", {
+
+export const login = (email, password) => 
+async(dispatch)=>{
+  const res = await fetch("/api/auth/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
       email,
-      password,
-    }),
-  });
-  return await response.json();
+      password
+    })
+    })
+    const data = await res.json()
+    if(!data.errors) dispatch(setSessionUser(user))
+  return data
 };
 
-export const logout = async () => {
+export const logout = () =>async (dispatch) => {
   const response = await fetch("/api/auth/logout", {
     headers: {
       "Content-Type": "application/json",
     },
   });
-  return await response.json();
+  dispatch(removeSessionUser()) 
+  return response
 };
-
-export const signUp = async (username, email, password) => {
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password } = user;
   const response = await fetch("/api/auth/signup", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
     body: JSON.stringify({
       username,
       email,
       password,
     }),
   });
-  return await response.json();
+    dispatch(setSessionUser(response.data.user));
+  return response;
 };
 
+// Reducer configuration
+
+const userTemplate = {
+  id: null,
+  username: null,
+  email: null,
+};
+const reducer = (state={user: userTemplate}, {type, payload}) => {
+  switch(type) {
+    case SET_SESSION_USER:
+      return {user: {...state.user, ...payload}};
+
+    case REMOVE_SESSION_USER:
+      return {user: {...state.user, ...payload}};
+    default:
+      return state; }
+};
 
 
