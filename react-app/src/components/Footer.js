da36e284091b0166e5fb25e5365b0785c1f487b1
Benjamin Lavian <benylavian@gmail.com>
2021-03-03 16:57:11 -0500

    added githib link but it is tiny

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -1,10 +1,14 @@
 import React from "react"
 import{Box} from "../styled/Footer"
+import { SiGithub } from "react-icons/si";
 
 function Footer(){
     return (
       <Box>
         <h1> developed by Beny Lavian</h1>
+        <a href="https://github.com/blavian/MySafePlace">
+          <SiGithub />
+        </a>
       </Box>
     );
     }
