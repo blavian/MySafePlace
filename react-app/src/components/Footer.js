eaab552beb7585448653437bdc8cb77d50561ad9
Benjamin Lavian <benylavian@gmail.com>
2021-03-03 16:03:23 -0500

    footer is sticking

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -1,8 +1,20 @@
 import React from "react"
+import styled from "styled-components"
 
+const Box = styled.div`
+  margin-top: 1rem;
+  padding: 1rem;
+  background-color:#ffff;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+`;
 function Footer(){
     return (
-    <h1> does this work??</h1>
+    <Box>
+    <h1> developed by Beny Lavian</h1>
+    </Box>
         )
     }
 
