aca81e1c23a03a4ef6348133d87419f368deb0f1
Benjamin Lavian <benylavian@gmail.com>
2021-03-03 16:42:54 -0500

    ready for now

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -1,21 +1,12 @@
 import React from "react"
-import styled from "styled-components"
+import{Box} from "../styled/Footer"
 
-const Box = styled.div`
-  margin-top: 1rem;
-  padding: 1rem;
-  background-color:#ffff;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-`;
 function Footer(){
     return (
-    <Box>
-    <h1> developed by Beny Lavian</h1>
-    </Box>
-        )
+      <Box>
+        <h1> developed by Beny Lavian</h1>
+      </Box>
+    );
     }
 
 export default Footer
---
+++ react-app/src/styled/Footer.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Box = styled.div`
+  margin-top: 0;
+  background-color: #ffff;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+`;
