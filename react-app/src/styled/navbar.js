7c6585d8cad67e1424248943b31cc028f5d42b06
Benjamin Lavian <benylavian@gmail.com>
2021-03-03 12:45:21 -0500

    basic navbar

---
+++ react-app/src/styled/navbar.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const navTop = styled.div`
+  background-color: #ffff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 6em;
+  letter-spacing: 6px;
+  text-transform: uppercase;
+`;
+
+
+
+
+export default navTop
