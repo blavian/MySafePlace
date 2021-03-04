7004b397608a17f28aea8757450d9f8b265145a4
Benjamin Lavian <benylavian@gmail.com>
2021-03-03 21:16:33 -0500

    changed notebook model not be unique

--- app/models/notebook.py
+++ app/models/notebook.py
@@ -5,7 +5,7 @@ class Notebook(db.Model):
   __tablename__ = 'notebooks'
 
   id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(40), nullable=False, unique=True)
+  title = db.Column(db.String(40), nullable=False)
   user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
 
