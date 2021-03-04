b6eff0ea9b628782a42c280fbb2025e00287def0
Benjamin Lavian <benylavian@gmail.com>
2021-03-04 12:33:26 -0500

    api route for updating notebooks

--- app/api/notebook_routes.py
+++ app/api/notebook_routes.py
@@ -35,7 +35,7 @@ def new_notebook():
 
     # 7. Send 201 response to the user
     return {"message": "success", "data": notebook.to_dict()}, 201
-# Get all of the current user's notebooks
+# Read all of the current user's notebooks
 
 
 @notebook_routes.route('')
@@ -43,7 +43,30 @@ def new_notebook():
 def get_notebooks():
     # get the user from the session
     user = current_user
-    # finds all of the user's notebooks based off of their userId
+    #  finds all of the user's notebooks based off of their userId
     user_notebooks = Notebook.query.filter(Notebook.user_id == user.id)
     # return list of all the notebooks
     return {"message": "success", "data": [notebook.to_dict() for notebook in user_notebooks]}, 200
+# Update a user's notebook
+
+
+@notebook_routes.route('/<int:id>', methods=['PATCH'])
+@login_required
+def update_notebook(id):
+    # create the form
+    form = NotebookForm()
+    # add csrf token to the form
+    form['csrf_token'].data = request.cookies['csrf_token']
+    # find notebook by its id
+    notebook = Notebook.query.get(id)
+    # Validate the form's data; if invalid return 400 bad request to user
+    if not form.validate_on_submit():
+        return {"message": "validation_errors", "data": form.errors}, 400
+    # if the form is valid,grab the title from the form
+    title = form.data["title"]
+    # update the title to what the user writes
+    notebook.title = title
+    # commit the changes to the database
+    db.session.commit()
+    # Return a  message with the updated notebook and a 201 response
+    return {"message": "success", "data": notebook.to_dict()}, 201
