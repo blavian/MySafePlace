b63507d2d300195f4be70ad09d732917bd93f63b
Benjamin Lavian <benylavian@gmail.com>
2021-03-04 14:27:25 -0500

    create notebook reducer first attempt

--- react-app/src/redux-store/notebook.js
+++ react-app/src/redux-store/notebook.js
@@ -4,30 +4,56 @@ const READ_NOTEBOOK = "READ_NOTEBOOK"
 const UPDATE_NOTEBOOK = "UPDATE_NOTEBOOK"
 const DELETE_NOTEBOOK = "DELETE_NOTEBOOK"
 
+//Action Creator
+const createNotebookAction = (payload)=>({
+    type:CREATE_NOTEBOOK,
+    payload
+})
+const readNotebookAction = (payload)=>({
+    type:READ_NOTEBOOK,
+    payload
+})
+const updateNotebookAction = (payload)=>({
+    type:UPDATE_NOTEBOOK,
+    payload
+})
+const DeleteNotebookAction = (payload)=>({
+    type:DELETE_NOTEBOOK,
+    payload
+})
+
+//THUNKS
+export const createNotebook =(title)=>async(dispatch)=>{
+    const res = await fetch("/api/notebooks", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            title
+        })
+    })
+    const {data} = res.data
+    dispatch(createNotebookAction(data))
+    return data  
+}
+
 //NOTEBOOK INITIAL STATE 
 const initialState = {
     title:[]
 }
-//Action Creator
-const createNotebook = (payload)=>({
-type:CREATE_NOTEBOOK,
-payload
-})
-const readNotebook = (payload)=>({
-type:READ_NOTEBOOK,
-payload
-})
-const updateNotebook = (payload)=>({
-type:UPDATE_NOTEBOOK,
-payload
-})
-const DeleteNotebook = (payload)=>({
-type:DELETE_NOTEBOOK,
-payload
-})
+const reducer =(state = {notebook:initialState},{type,payload}
+    )=>{
+        let stateCopy
+        switch (type) {
+          case CREATE_NOTEBOOK:
+            stateCopy = { title: { ...state.title } };
+            return stateCopy;
+          default:
+            return state;
+        }
+    }
 
-//THUNKS
-
-
+export default reducer
 
 
