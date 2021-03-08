dd7f37a02376f70031e5ce1b0efdd0ab503b1759
Benjamin Lavian <benylavian@gmail.com>
2021-03-08 15:10:26 -0500

    getting there...

--- react-app/src/components/Notebook.js
+++ react-app/src/components/Notebook.js
@@ -5,47 +5,52 @@ import{getNotebook,createNotebook} from "../redux-store/notebook"
 import TopRight from "../styled/top-right";
 import Button from "../styled/button"
 import Center from "../styled/center"
- 
+import{Main,Cards,CardItems,Card,CardContent,Image,CardTitle} from "../styled/card"
 
  const Notebook = () => {
    const dispatch = useDispatch();
-   const [title,setTitle] = useState('')
-   const currentNotebooks = useSelector(state => Object.values(state.notebook))
-     
+   const [title, setTitle] = useState("");
+   const currentNotebooks = useSelector((state) =>
+     Object.values(state.notebook)
+   );
+
    useEffect(async () => {
      dispatch(getNotebook());
    }, [dispatch]);
-   
-   
-   const handleSubmit = async e =>{
-     e.preventDefault()
-     await dispatch(createNotebook(title))
-     setTitle("")
-   }
-   const updateTitle = e=>setTitle(e.target.value)
+
+   const handleSubmit = async (e) => {
+     e.preventDefault();
+     await dispatch(createNotebook(title));
+     setTitle("");
+   };
+   const updateTitle = (e) => setTitle(e.target.value);
    return (
-     <>
-       <Center> Notebooks </Center>
+     <div>
+     <Center>My Notebooks</Center>
        {currentNotebooks &&
          currentNotebooks.map((notebook) => {
-           return <div>{notebook.title}</div>;
+           return (
+             <>
+               <Main>
+                 <Cards>
+                   <CardItems>
+                     <Card>
+                       <Image>
+                         <img src="https://cdn.pixabay.com/photo/2016/08/03/09/00/self-esteem-1566153_960_720.jpg" />
+                       </Image>
+                       <CardContent>
+                         <CardTitle>{notebook.title}</CardTitle>
+                       </CardContent>
+                     </Card>
+                   </CardItems>
+                 </Cards>
+               </Main>
+             </>
+           );
          })}
-       <TopRight>
-       <form onSubmit={handleSubmit}>
-         <input
-           type="text"
-           value={title}
-           onChange={(e) => setTitle(e.target.value)}
-           name="title"
-         />
-         <Button type="submit">Add a new Notebook</Button>
-         {/* <Button onClick={(evt) => updateTitle(evt)} type="button">
-           Update
-         </Button> */}
-       </form>
-       </TopRight>
-     </>
+     </div>
    );
  };
+  
 
  export default Notebook
--- react-app/src/styled/card.js
+++ react-app/src/styled/card.js
@@ -1,26 +1,44 @@
 import styled from "styled-components"
 
-
-
-export const CardClass = styled.div`
+export const Main = styled.div`
+max-width:1000px;
+margin:0 auto;
+`
+export const Cards = styled.ul`
+    display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`
+export const CardItems = styled.li`
   display: flex;
-  height: 280px;
-  width: 200px;
-  background-color: #17141d;
-  border-radius: 10px;
-  box-shadow: -1rem 0 3rem #000;
-  transition: 0.4s ease-out;
-  position: relative;
-  left: 0px;
-  &:hover {
-    transform: translateY(-20px);
-    transition: 0.4s ease-out;
-  }
+  padding: 1rem;
 `;
-export const CardTitle = styled.div`
-  color: white;
-  font-weight: 300;
-  position: absolute;
-  left: 20px;
-  top: 15px;
+export const Card = styled.div`
+  background-color: white;
+  border-radius: 0.25rem;
+  box-shadow: 0 20px 40px -14px rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+`;
+export const Image = styled.div`
+  height: auto;
+  max-width: 10%;
+  vertical-align: middle;
+`;
+
+export const CardContent = styled.div`
+  padding: 1rem;
+  background: linear-gradient(to bottom left, #ef8d9c 40%, #ffc39e 100%);
+`;
+
+export const CardTitle = styled.h2`
+  color: #ffffff;
+  font-size: 1.1rem;
+  font-weight: 700;
+  letter-spacing: 1px;
+  text-transform: capitalize;
+  margin: 0px;
 `;
