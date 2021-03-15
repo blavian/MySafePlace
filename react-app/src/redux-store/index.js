99a4ac4ae69219f4ed4beaac824a86ec4c43a8a1
Benjamin Lavian <benylavian@gmail.com>
2021-03-14 23:43:27 -0400

    index.js for the store

--- react-app/src/redux-store/index.js
+++ react-app/src/redux-store/index.js
@@ -1,25 +1,12 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 import session from "./session";
-import notebook from "./notebook"
-const rootReducer = combineReducers({
-  session,
-  notebook
+import notebook from "./notebook";
+
+const store = configureStore({
+  reducer: {
+    session,
+    notebook,
+  },
 });
 
-let enhancer;
-
-if (process.env.NODE_ENV === "production") {
-  enhancer = applyMiddleware(thunk);
-} else {
-  const logger = require("redux-logger").default;
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
-}
-
-const configureStore = (preloadedState) => {
-  return createStore(rootReducer, preloadedState, enhancer);
-};
-
-export default configureStore;
+export default store;
