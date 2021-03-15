b0be949d7106e2e35fea1ae2c4d21d83fcd6ca0c
Benjamin Lavian <benylavian@gmail.com>
2021-03-15 02:30:57 -0400

    affirmation route in app.js

--- react-app/src/components/affirmation.js
+++ react-app/src/components/affirmation.js
@@ -1,113 +1,56 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  getAff,
-  createNotebook,
-  deleteNotebook,
+  getAffirmations 
 } from "../redux-store/affirmation"
-import TopRight from "../styled/top-right";
-import Button from "../styled/button";
-import Center from "../styled/center";
+
 import * as Card from "../styled/card";
 
-import Modal from "styled-react-modal";
-import EditForm from "./EditForm";
-
-const Notebook = () => {
+const Affirmations = () => {
   const dispatch = useDispatch();
 
   const [title, setTitle] = useState("");
-  const StyledModal = Modal.styled`
-  width: 50rem;
-  height: 50rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-  const [isOpen, setIsOpen] = useState(false);
-
-  function toggleModal(e) {
-    setIsOpen(!isOpen);
-  }
-  const currentNotebooks = useSelector((state) =>
-    Object.values(state.notebook)
+  const[description,setDescription] = useState('')
+  const[date,setDate] = useState('')
+  
+  const currentAffirmations = useSelector((state) =>
+    Object.values(state.affirmation)
   );
 
-  const [currentNotebook, setCurrentNotebook] = useState({ id: "", title: "" });
+ 
 
   useEffect(async () => {
-    dispatch(getNotebook());
+    dispatch(getAffirmations());
   }, [dispatch]);
 
-  const addNotebook = async (e) => {
-    e.preventDefault();
-    await dispatch(createNotebook(title));
-    setTitle("");
-  };
-
-  const deleted = async (e, notebookId) => {
-    await dispatch(deleteNotebook(notebookId));
-  };
 
   return (
     <div>
-      <Center>My Notebooks</Center>
-      <TopRight>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        name="title"
+      />
+      <input
+        type="text"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        name="description"
+        />
         <input
           type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          name="title"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          name="date"
         />
-        <Button onClick={(e) => addNotebook(e)} type="button">
-          Add Notebook
-        </Button>
-      </TopRight>
-      <StyledModal
-        isOpen={isOpen}
-        onBackgroundClick={toggleModal}
-        onEscapeKeydown={toggleModal}
-      >
-        <EditForm currentNotebook={currentNotebook} />
-        <Button onClick={toggleModal}>Close</Button>
-      </StyledModal>
-      {currentNotebooks &&
-        currentNotebooks.map((notebook) => {
+      {currentAffirmations &&
+        currentAffirmations.map((a) => {
           return (
-            <>
-              <Card.Main>
-                <Card.Cards>
-                  <Card.CardItems>
-                    <Card.Cards>
-                      <Card.Image>
-                        <img src="https://cdn.pixabay.com/photo/2016/08/03/09/00/self-esteem-1566153_960_720.jpg" />
-                      </Card.Image>
-                      <Card.CardContent>
-                        <Card.CardTitle>{notebook.title}</Card.CardTitle>
-                        <Card.CardButton
-                          onClick={(e) => {
-                            setCurrentNotebook({
-                              id: notebook.id,
-                              title: notebook.title,
-                            });
-                            setIsOpen(true);
-                          }}
-                        >
-                          edit title
-                        </Card.CardButton>
-                        <Card.CardButton
-                          onClick={(e) => {
-                            deleted(e, notebook.id);
-                          }}
-                        >
-                          Delete
-                        </Card.CardButton>
-                      </Card.CardContent>
-                    </Card.Cards>
-                  </Card.CardItems>
-                </Card.Cards>
-              </Card.Main>
-            </>
+            <div key={affirmation.id}>
+             <p>{a.title}</p>
+             
+            </div>
           );
         })}
     </div>
