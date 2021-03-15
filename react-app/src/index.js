20cf476b8c257078adc56225741a24d069a73104
Benjamin Lavian <benylavian@gmail.com>
2021-03-14 23:39:56 -0400

    index.js in src folder

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,20 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
-import configureStore from './redux-store'
+import store from './redux-store'
 
-import * as sessionActions from './redux-store/session'
-import * as notebookActions from './redux-store/notebook'
 
-//Make a store
-const store = configureStore();
 
- //Make redux available in development
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
-  window.sessionActions = sessionActions
-  window.notebookActions = notebookActions
-}
+
 
 
 ReactDOM.render(
