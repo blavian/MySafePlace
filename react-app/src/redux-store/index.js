16f499af7405697f7fe115e9f4f42f65adddf71e
Benjamin Lavian <benylavian@gmail.com>
2021-03-15 00:13:23 -0400

    import notebooksReducer

--- react-app/src/redux-store/index.js
+++ react-app/src/redux-store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import session from "./session";
 import notebook from "./notebook";
+import notebooksReducer from "../features/Notebooks/notebookSlice"
 
 const store = configureStore({
   reducer: {
     session,
     notebook,
+    notebooks:notebooksReducer
   },
 });
 
