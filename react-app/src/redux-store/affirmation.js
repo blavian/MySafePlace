50ee50e63cb3a5dabac2f35b48258d42a3f35c22
Benjamin Lavian <benylavian@gmail.com>
2021-03-15 01:49:56 -0400

    thunk

--- react-app/src/redux-store/affirmation.js
+++ react-app/src/redux-store/affirmation.js
@@ -6,3 +6,17 @@ const setAffirmation = (payload) => ({
   type: SET_AFFIRMATION,
   payload,
 });
+
+//THUNKS
+export const getNotebook = (id) => async (dispatch) => {
+  const response = await fetch("/api/affirmations", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    let { data } = await response.json();
+
+    dispatch(setAffirmation(data));
+  }
+};
