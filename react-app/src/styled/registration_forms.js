53c9ca14fdfae53d681c3bc907d8e3e5888842d4
Benjamin Lavian <benylavian@gmail.com>
2021-03-14 22:40:44 -0400

    sign up form

---
+++ react-app/src/styled/registration_forms.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
