7a6fd177155c0f6697503b88c6b012de60262d88
Benjamin Lavian <benylavian@gmail.com>
2021-03-20 23:32:36 -0400

    background-image is responsive

--- react-app/src/components/LandingPage.js
+++ react-app/src/components/LandingPage.js
@@ -1,16 +1,18 @@
-import React from "react"
-
-
+ import React from "react"
  import styled from "styled-components";
  import img from "../images/self-esteem.jpg"
 
- const MainImage = styled.div`
-   background-image: url(${img});
-   background-size: cover;
-   height: 60rem;
-   box-sizing: border-box;
-   margin: 0;
- `;
+
+
+const MainImage = styled.div`
+  background-image: url(${img});
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  height: 60rem;
+  box-sizing: border-box;
+`;
 
 function LandingPage(){
     return (
