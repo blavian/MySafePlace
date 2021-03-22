0db5dbd6ea769d5d6ba964640fe4f8654a973a7e
Benjamin Lavian <benylavian@gmail.com>
2021-03-22 17:01:29 -0400

    centered

--- react-app/src/components/affirmation.js
+++ react-app/src/components/affirmation.js
@@ -2,6 +2,7 @@ import React, {useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAffirmations } from "../redux-store/affirmation";
 import { useParams } from "react-router-dom";
+import Center from "../styled/center"
 
 
  
@@ -19,7 +20,7 @@ import { useParams } from "react-router-dom";
 
     return (
       <div>
-        <h1>My Affirmations</h1>
+        <Center>My Affirmations</Center>
         {!!currentAffirmation && currentAffirmation.map((affirmation)=>{
           return(
             <>
