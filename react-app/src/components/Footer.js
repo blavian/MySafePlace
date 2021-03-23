ca175d885dba5d75009c4df00e9fea0e806685c1
Benjamin Lavian <benylavian@gmail.com>
2021-03-22 22:42:32 -0400

    updated footer

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -1,16 +1,21 @@
-import React from "react"
-import{Box} from "../styled/Footer"
-import { SiGithub } from "react-icons/si";
+import React from "react";
+import { Box, Content } from "../styled/Footer"
+import { SiGithub, SiLinkedin } from "react-icons/si";
 
-function Footer(){
-    return (
-      <Box>
-        {/* <h1> developed by Beny Lavian</h1> */}
+function Footer() {
+  return (
+    <Box>
+      <Content>
+        <p>Developed By Beny Lavian</p>
         <a href="https://github.com/blavian/MySafePlace">
-          <SiGithub />
+          <SiGithub size="32px" />
         </a>
-      </Box>
-    );
-    }
+        <a href="https://www.linkedin.com/in/benjamin-lavian/">
+          <SiLinkedin size="32px" />
+        </a>
+      </Content>
+    </Box>
+  );
+}
 
-export default Footer
+export default Footer;
