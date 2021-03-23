19be1d0e7fb16392b1d95ab4113888b85349e982
Benjamin Lavian <benylavian@gmail.com>
2021-03-23 16:28:31 -0400

    Redo

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GlobalStyle from "./globalStyles"
+
 import App from './App';
 import { Provider } from "react-redux";
 import configureStore from './redux-store'
@@ -22,7 +22,6 @@ if (process.env.NODE_ENV !== 'production') {
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <GlobalStyle />
       <App />
     </Provider>
   </React.StrictMode>,
