dbe538f3c29e5e61e010caf5c3d338b8bc939d31
Benjamin Lavian <benylavian@gmail.com>
2021-03-23 16:24:19 -0400

    put global styles in the correct place

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import GlobalStyle from "./globalStyles"
 import App from './App';
 import { Provider } from "react-redux";
 import configureStore from './redux-store'
@@ -21,9 +21,10 @@ if (process.env.NODE_ENV !== 'production') {
 
 ReactDOM.render(
   <React.StrictMode>
-  <Provider store = {store}>
-    <App />
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
