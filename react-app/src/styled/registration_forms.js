c2ca19e025cd411419a979cc618f387bf90bcf2d
Benjamin Lavian <benylavian@gmail.com>
2021-03-31 14:51:31 -0400

    padding

--- react-app/src/styled/registration_forms.js
+++ react-app/src/styled/registration_forms.js
@@ -4,13 +4,12 @@ import styled from "styled-components"
 export const Container = styled.div`
   display: flex;
   justify-content: center;
-  padding:100px;
+  padding: 8rem;
 `;
 
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  padding:100px;
 `;
 
