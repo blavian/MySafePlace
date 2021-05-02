a11d45cecf0f652a64a837e8c151b1dfb45b5c03
Benjamin Lavian <benylavian@gmail.com>
2021-05-02 11:25:02 -0400

    added back image

--- react-app/src/styled/card.js
+++ react-app/src/styled/card.js
@@ -1,20 +1,19 @@
-import styled from "styled-components"
-
+import styled from "styled-components";
 
 export const Main = styled.div`
-max-width:auto;
-margin:0 auto;
-`
+  max-width: auto;
+  margin: 0 auto;
+`;
 export const Cards = styled.ul`
   margin: 0;
   padding: 0;
-`
+`;
 export const CardItems = styled.li`
   display: flex;
   padding: 1rem;
 `;
 export const Card = styled.div`
-  background-color: #20bf55;
+    background-color #20bf55;
   border-radius: 0.25rem;
   display: flex;
   flex-direction: column;
@@ -30,7 +29,6 @@ export const CardContent = styled.div`
   padding: 1rem;
     background-color #20bf55;
     background-image linear-gradient(315deg, #20bf55 0%, #01baef 74%);
-
 `;
 
 export const CardTitle = styled.h2`
@@ -39,13 +37,13 @@ export const CardTitle = styled.h2`
   font-weight: 600;
   letter-spacing: 2px;
   text-transform: uppercase;
-  text-align:center;
+  text-align: center;
   margin: 0px;
 `;
 export const CardButton = styled.a`
-   color: hsl(240, 92%, 65%);
+  color: blue;
   padding: 0.8rem;
-  font-size: 1em;
+  font-size: 14px;
   text-transform: uppercase;
   border-radius: 4px;
   font-weight: 400;
