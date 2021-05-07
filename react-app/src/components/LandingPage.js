3c33a96b99d7fa58a9b95f410e259e8d760d1b91
Benjamin Lavian <benylavian@gmail.com>
2021-05-07 11:45:11 -0400

    removed container

--- react-app/src/components/LandingPage.js
+++ react-app/src/components/LandingPage.js
@@ -1,12 +1,9 @@
  import React from "react"
  import styled from "styled-components";
- import img from "../images/self-esteem.jpg"
 
 
-const Container = styled.div`
-display: flex;
-flex-direction:column;
-`
+
+
 
 const MainImage = styled.img`
   padding: 10px;
@@ -17,12 +14,13 @@ const MainImage = styled.img`
 
 function LandingPage(){
     return (
-      <Container>
+     
         <MainImage
           src="https://cdn.pixabay.com/photo/2016/08/03/09/00/self-esteem-1566153_960_720.jpg"
           alt="No image found"
         ></MainImage>
-      </Container>
+           
+      
     );
     }
 
