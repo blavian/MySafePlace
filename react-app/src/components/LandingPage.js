a14b4a706d28b9025472c286ffd9bc1156f1b1b7
Benjamin Lavian <benylavian@gmail.com>
2021-05-07 11:35:03 -0400

    image is responsive

--- react-app/src/components/LandingPage.js
+++ react-app/src/components/LandingPage.js
@@ -3,21 +3,27 @@
  import img from "../images/self-esteem.jpg"
 
 
+const Container = styled.div`
+display: flex;
+flex-direction:column;
+`
 
-const MainImage = styled.div`
-  background-image: url(${img});
-  background-position: center center;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-size: cover;
-  height: 60rem;
-  box-sizing: border-box;
+const MainImage = styled.img`
+  padding: 10px;
+  width: 100%;
+  height: 100vh;
+  object-fit: cover;
 `;
 
 function LandingPage(){
     return (
-        <MainImage></MainImage>
-        )
+      <Container>
+        <MainImage
+          src="https://cdn.pixabay.com/photo/2016/08/03/09/00/self-esteem-1566153_960_720.jpg"
+          alt="No image found"
+        ></MainImage>
+      </Container>
+    );
     }
 
 export default LandingPage
